2c59a94175ee3553bd2945ee80391bf54e02b2ec
anushree <anushree.subramani@tcs.com>
2016-03-15 03:00:03 +0530

    Second commmit

--- finalproject.py
+++ finalproject.py
@@ -9,23 +9,15 @@ engine = create_engine('postgresql:///catalog')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
+
 session = DBSession()
-
-
-# Fake Catalogs
-# catalog = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-# catalogs = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-# Fake item Items
-# items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-# item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-# items = []
 
 
 @app.route('/catalog/<int:catalog_id>/item/JSON')
 def catalogitemJSON(catalog_id):
+    DBSession = sessionmaker(bind=engine)
+
+    session = DBSession()
     catalog = session.query(Catalog).filter_by(id=catalog_id).one()
     items = session.query(Item).filter_by(
         catalog_id=catalog_id).all()
@@ -56,6 +48,9 @@ def showCatalogs():
 # Create a new catalog
 @app.route('/catalog/new/', methods=['GET', 'POST'])
 def newCatalog():
+    DBSession = sessionmaker(bind=engine)
+
+    session = DBSession()
     if request.method == 'POST':
         newCatalog = Catalog(name=request.form['name'])
         session.add(newCatalog)
